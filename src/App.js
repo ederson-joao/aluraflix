92af0bad1d9509a58dbfdc34dbc845ea18f73633
Ederson João <ederson.joao@outlook.com>
2024-07-16 19:29:11 -0300

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header/index';
+import Category from './components/categories/index';
+import AddVideoForm from './components/newVideo/index';
+import Footer from './components/footer/index';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="" element={<Category />} />
+          <Route path="/newvideo" element={<AddVideoForm />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
